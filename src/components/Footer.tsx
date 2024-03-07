2895f46b9f04098bb990d7e6a02eba4dd9219c12
Mary Angel <201910994@fit.edu.ph>
2024-03-07 18:54:17 +0800

    footer - grain bg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,11 +8,12 @@ import { FaSquareFacebook } from "react-icons/fa6"
 const Footer = () => {
   return (
     <Container id="contact" className="bg-[#6895AB] w-full">
+        <div id='background' className='absolute opacity-35 w-full h-full -z-1'></div>
         <div className="flex flex-col justify-center items-start laptop:items-center gap-4 w-full pt-6 pb-0 laptop:py-20 font-arsenal text-[#fafafa]">
             <div className="flex flex-col laptop:items-center gap-1 w-full">
                 <div className="text-sm laptop:text-md">Got a project?</div>
                 <div className="">
-                    <div className="font-cardo text-[10vw] desktop:text-[11vw] leading-none mb-4">GET IN TOUCH</div>
+                    <div className="font-cardo text-[10vw] desktop:text-[11vw] leading-none mb-4 laptop:mb-0">GET IN TOUCH</div>
                     <div className="flex flex-col laptop:flex-row justify-between items-start gap-28 w-full">
                         <div className="flex flex-col justify-end gap-4">
                             <div className="flex flex-col gap-1">
@@ -39,30 +40,6 @@ const Footer = () => {
                 </div>
                 
             </div>
-            
-            {/* <div className="flex flex-col laptop:flex-row justify-between items-start gap-28 w-full">
-                <div className="flex flex-col justify-end gap-3">
-                    <div className="flex flex-col gap-2">
-                        <div className="flex items-center gap-2"><MdOutlineMailOutline /> {imergeData.footer.contact.email} </div>
-                        <div className="flex items-center gap-2"><MdOutlineSmartphone /> {imergeData.footer.contact.mobile} </div>
-                    </div>
-                    
-                    <hr className="w-10" />
-
-                    <div>
-                        <div className="flex items-center gap-2"><FaSquareFacebook /> {imergeData.footer.social.facebook} </div>
-                        <div className="flex items-center gap-2"><FaInstagram /> {imergeData.footer.social.instagram} </div>
-                    </div>
-                    
-                    <hr className="w-10" />
-
-                    <div className="flex items-center gap-2"><SlLocationPin /> {imergeData.footer.address} </div>
-                </div>
-
-                <div>
-                    © 2024 iMerge. All rights reserved.
-                </div>
-            </div> */}
         </div>
     </Container>
   )
